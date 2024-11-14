ae4e0f58ed13e24ef703d305b36464d19cf7b27f
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2024-11-14 20:29:47 +0530

    added count api for dimension values

--- dynamics/apis/dimensions.py
+++ dynamics/apis/dimensions.py
@@ -6,6 +6,8 @@ class Dimensions(ApiBase):
 
     GET_DIMENSION = '/dimensions'
     GET_DIMENSION_VALUES = '/dimensions({0})/dimensionValues'
+    COUNT_DIMENSIONS = '/dimensions/$count'
+    COUNT_DIMENSIONS_VALUES = '/dimensions({0})/dimensionValues/$count'
 
     def get_all_dimensions(self, **kwargs):
         """
@@ -16,7 +18,7 @@ class Dimensions(ApiBase):
             **kwargs
         }, Dimensions.GET_DIMENSION)['value']
 
-    def get_all_dimension_values(self, dimension_name:str, **kwargs):
+    def get_all_dimension_values(self, dimension_id:str, **kwargs):
         """
         Get all dimension_values
 
@@ -27,4 +29,23 @@ class Dimensions(ApiBase):
         """
         return self._get_request({
             **kwargs
-        }, (Dimensions.GET_DIMENSION_VALUES).format(dimension_name))['value']
+        }, (Dimensions.GET_DIMENSION_VALUES).format(dimension_id))['value']
+
+    def count(self, **kwargs):
+        """
+        Get counts of dimensions
+        :return: Count in Int
+        """
+        return self._get_request_for_count({
+            **kwargs
+        }, Dimensions.COUNT_DIMENSIONS)['value']
+
+    def count_dimension_values(self, destination_id:str, **kwargs):
+        """
+        Get counts of dimensions values with the given destination_id
+        :return: Count in Int
+        """
+
+        return self._get_request_for_count({
+            **kwargs
+        }, (Dimensions.COUNT_DIMENSIONS_VALUES).format(destination_id))['value']
