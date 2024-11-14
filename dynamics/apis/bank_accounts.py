ee6189e4e8f44ea1acca6e3449f89740be3fafad
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2024-11-14 20:14:06 +0530

    added count for bank account

--- dynamics/apis/bank_accounts.py
+++ dynamics/apis/bank_accounts.py
@@ -5,6 +5,7 @@ class BankAccounts(ApiBase):
     """Class for Bank Accounts APIs."""
 
     GET_BANK_ACCOUNT = '/bankAccounts'
+    COUNT_BANK_ACCOUNT = '/bankAccounts/$count'
 
     def get_all(self, **kwargs):
         """
@@ -14,3 +15,12 @@ class BankAccounts(ApiBase):
         return self._get_request({
             **kwargs
         }, BankAccounts.GET_BANK_ACCOUNT)['value']
+
+    def count(self, **kwargs):
+        """
+        Get counts of locations
+        :return: Count in Int
+        """
+        return self._get_request_for_count({
+            **kwargs
+        }, BankAccounts.COUNT_BANK_ACCOUNT)['value']
